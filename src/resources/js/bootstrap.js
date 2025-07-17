66ca1d894243c346690ba0b9260bc815cab8d020
Kentillation <kentanthony070495@gmail.com>
2025-07-17 10:51:10 +0800

    July 17, 2025

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -3,10 +3,19 @@ import Pusher from 'pusher-js';
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
+    key: process.env.VUE_APP_PUSHER_KEY || 'f2ff9925065555c61de5',
+    cluster: process.env.VUE_APP_PUSHER_CLUSTER || 'ap1',
+    wsHost: process.env.VUE_APP_PUSHER_HOST, // remove in Production
+    wsPort: process.env.VUE_APP_PUSHER_PORT, // remove in Production
+    wssPort: process.env.VUE_APP_PUSHER_PORT, // remove in Production
+    forceTLS: process.env.VUE_APP_PUSHER_FORCE_TLS,
+    encrypted: process.env.VUE_APP_PUSHER_ENCRYPTED,
+    disableStats: true,
     enabledTransports: ['ws', 'wss'],
+    authEndpoint: '/broadcasting/auth', // for local development 
+    auth: {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('auth_token')}`
+        }
+    }
 });
