18e06a98945937631941a6dec08ff4ab48b78f59
Kentillation <kentanthony070495@gmail.com>
2025-07-25 11:33:54 +0800

    July 25, 2025

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <v-container>
-    <v-snackbar v-model="snackbar.visible" :color="snackbar.color" location="top" class="mt-9" timeout="10000"
+    <v-snackbar v-model="snackbar.visible" :color="snackbar.color" location="top" class="mt-10" timeout="10000"
       persistent>
       <div class="d-flex align-items-center px-3">
         <span style="font-size: 18px;"><v-icon icon="mdi-information-outline"></v-icon></span>
